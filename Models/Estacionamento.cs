0b6bc3c708e2c5ba6dd004885ce9687d5b2103de
GabrielAstra <gvlc2013@gmail.com>
2023-07-07 16:17:21 -0300

    desagio

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,63 @@
+namespace DesafioFundamentos.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placaVeiculo = Console.ReadLine();
+
+            veiculos.Add(placaVeiculo);
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+
+            string placaVeiculo = Console.ReadLine();
+            
+            if (veiculos.Any(x => x.ToUpper() == placaVeiculo.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                int horasEstacionado = int.Parse(Console.ReadLine());
+
+                decimal valorTotal = precoInicial + (precoPorHora * horasEstacionado);
+
+                veiculos.Remove(placaVeiculo);
+
+                Console.WriteLine($"O veículo {placaVeiculo} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+ 
+                foreach (string placaVeiculo in veiculos)
+                {
+                    Console.WriteLine(placaVeiculo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
